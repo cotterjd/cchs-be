b98e74a6fedb0d6c53652f276e7d5593e3dd58d0
Jordan Cotter <jrdn.cttr@gmail.com>
2022-08-31 14:27:27 -0500

    make cors handle multiple domains

--- src/utils/cors.ts
+++ src/utils/cors.ts
@@ -1,9 +1,13 @@
 export const cors = (req, res, next) => {
   res.header({
-    "Access-Control-Allow-Origin": process.env.FE_URL,
     "Access-Control-Allow-Headers": "Content-Type",
     "Access-Control-Allow-Methods": ["GET", "POST", "PUT", "DELETE"]
   });
+  const origin = req.headers.origin || "*"
+  const allowedOrigins = process.env.FE_URL.split(`,`) 
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin)
+  }
   if (req.method === "OPTIONS") return res.end("");
   next();
 }
